ee5e5d20dfe482ed90d8097c21bab1d6830dee51
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-05-16 23:56:26 -0300

    commit 16/05/23

---
+++ bin/dart_exceptions.dart
@@ -0,0 +1,33 @@
+import 'controllers/bank_controller.dart';
+import 'models/Account.dart';
+
+void main() {
+
+  //Criação do banco
+
+  BankController bankController = BankController();
+
+
+  //Criação de contas
+  
+  bankController.addAccount(
+      id: "Ricarth",
+      account: Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true)
+  );
+
+  bankController.addAccount(
+      id: "Kako",
+      account: Account(name: "Caio Couto", balance: 600, isAuthenticated: true)
+  );
+
+
+  //Efetuando transferência
+
+  bool result = bankController.makeTransfer(idSender: "Kako", idReceiver: "Ricarth", ammount: 700);
+
+
+  //Printando resultado
+  print(result);
+
+
+}
