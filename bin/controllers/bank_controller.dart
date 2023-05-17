2e553f3f798b3c9b8b632b6e771da8fd13d5b1c4
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-05-16 23:34:41 -0300

    commit 15/05/23

---
+++ bin/controllers/bank_controller.dart
@@ -0,0 +1,44 @@
+import '../models/Account.dart';
+
+class BankController {
+
+  //classe responsável por gerenciar conta e fazer as transações entre elas
+
+  final Map <String, Account> _database = {}; //Armazenamento da instância do "banco de dados" que gerencia contas cadastradas
+
+  //construtor de adição de conta:
+  addAccount( { required String id, required Account account } ) { _database[id] = account;}
+
+
+  //Método de verificação de id:
+  bool verifyId (String id) {
+    return _database.containsKey(id);
+  }
+
+
+
+//método para transação bancária entre duas contas com verificações (validade de IDs, autenticação dos usuários, saldo disponível etc)
+  bool makeTransfer ( { required String idSender, required String idReceiver, required double ammount } ) {
+
+    { //verificação se ID de remetente é válido
+      if (!verifyId(idSender)) {
+        return false;
+      }
+    }
+
+    { //verificação se ID de destinatário é válido
+      if (!verifyId(idReceiver)) {
+        return false;
+      }
+    }
+
+    Account accountSender = _database[idSender]!;
+    Account accountReceiver = _database[idReceiver];
+
+  }
+
+
+}
+
+
+
--- bin/models/account.dart
+++ bin/models/account.dart
@@ -1,18 +1,14 @@
 class Account {
 
+  //classe responsável por representar uma conta de banco dos clientes
+
   //propriedades:
   String name;
   double balance;
   bool isAuthenticated;
 
   //construtor:
-  Account(
-    {
-      required this.name,
-      required this.balance,
-      required this.isAuthenticated
-    }
-  );
+  Account( { required this.name, required this.balance, required this.isAuthenticated } );
 
   //método para editar saldo:
   editBalance({required value}) {
